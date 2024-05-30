0cfd4c6b3c59fbd21040a795d4476b5c82901e8a
Hi EveryOne, I Am Sheraz Irshad! <134085129+sherazirshad680@users.noreply.github.com>
2024-05-31 03:40:53 +0800

    CLASS-02-CODE

---
+++ PIAIC CLASSES/(PIAIC) TS Class 2/index.ts
@@ -0,0 +1,70 @@
+// console.log("Hello world!");
+// var a: number = 20;
+// var b = "Sheraz";
+// console.log(b);
+
+// Store your name and age in a variable and show as following
+
+// My name is Sheraz and I am 16 years old.
+
+var userName: string = "Sheraz";
+var userAge: number = 16;
+
+console.log("My name is", userName, "I am", userAge, "years old.");
+
+///////////////////////////////////////
+
+console.log("Hello World");
+var a: number = 10;
+var b: number = 20;
+var c: number = 30;
+var _name = "Sheraz";
+console.log(_name, "a =", a, "b =", b, "c =", c);
+
+//Operator
+// Arithmetic Operators
+// +, -, *, /, %
+
+console.log(10 + 290);
+
+var sum = 10 + 20 + 30 + 40;
+var diff = sum - 30;
+var product = 40 * 30;
+var div = 40 / 30;
+console.log(sum);
+console.log(diff);
+console.log(product);
+console.log(div);
+
+var remaining = 13 % 7;
+console.log(remaining);
+
+// Exponent Operator
+// **
+var cube = 27 ** (1 / 3);
+console.log(cube);
+
+// Increment && Decrement operators
+
+var value = 10;
+value = value + 1;
+var val2 = value++;
+val2 = value;
+value = value + 1;
+
+var val3 = ++value;
+value = value +1
+val3 = value
+
+// where value = 10
+var sum = value + ++val3 + val2++;
+12 +13 +10
+console.log(sum);
+// 34,35,30
+var a = 15;
+var b = 10;
+var result: number = --a + ++a - --b + b;
+14+15 -9+9
+console.log(result);
+b= b-1
+b = b;
