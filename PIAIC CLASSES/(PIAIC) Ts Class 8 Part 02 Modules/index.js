8fb36bb2bb28eac49dab3f8c4cc01d66ebeb9862
Hi EveryOne, I Am Sheraz Irshad! <134085129+sherazirshad680@users.noreply.github.com>
2024-05-31 04:17:21 +0800

    CLASS-08-CODE

---
+++ PIAIC CLASSES/(PIAIC) Ts Class 8 Part 02 Modules/index.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.class1 = exports.sum = exports.friendName = exports.teacherName = exports.myName = void 0;
+// export
+// export default
+exports.myName = "Abu Hurairah";
+exports.teacherName = "Naveed Sarwar";
+exports.friendName = "Ahmad Sultan";
+const sum = () => 10 + 20;
+exports.sum = sum;
+exports.class1 = {
+    name: "Batch 60",
+    strength: 150,
+    sum: (num1) => {
+        console.log(num1 + 10);
+    },
+};
+const myName2 = ["Abu Hurairah", "Naveed Sarwar"];
+exports.default = myName2;
+const myName3 = ["Abu Hurairah", "Naveed Sarwar"];
---
+++ PIAIC CLASSES/(PIAIC) Ts Class 8 Part 02 Modules/order.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const index_js_1 = require("./index.js");
+console.log(index_js_1.myName);
+index_js_1.class1.sum(10);
+const myName = "Sheraz";
